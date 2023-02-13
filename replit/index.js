65edef00ac54a8dffbcb9f928b941484b2597977
thisis realname <65318266+Avad3@users.noreply.github.com>
2023-02-13 14:02:51 +0000

    Add comment

--- replit/index.js
+++ replit/index.js
@@ -1,3 +1,10 @@
+/*
+  Search does NOT work with this current version. Support will be added later.
+  Everything else is 100% functional.
+
+  The original code can be found at https://github.com/retronbv/buffy
+*/
+
 import http from 'node:http';
 import fetch from 'node-fetch';
 
@@ -6,11 +13,11 @@ const url = "https://kazwire.com";
 const PORT = process.env.PORT || 3000;
 
 server.on('request', async (req, res) => {
-  const reqUrl = req.url === "*" ? "/" : req.url
+  const reqUrl = req.url === "*" ? "/" : req.url;
   const asset = await fetch(url + reqUrl);
   const body = new Buffer.from(await asset.arrayBuffer());
   res.writeHead(asset.status, { "Content-Type": asset.headers.get("content-type").split(";")[0] });
   res.end(body);
 });
 
-server.listen(PORT)
+server.listen(PORT);
